8a410984260d351d5c2d451fff3e19dc2fb6877b
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-01-05 14:47:15 +0000

    Done

---
+++ src/Components/Keypad.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Column from 'react-bootstrap/Column'
+
+function Keypad() {
+    
+    return ( 
+
+    <Container>
+    <Row>
+        <Col xs="8"></Col>
+        <Col></Col>
+        <Col></Col>
+    </Row>
+    <Row>
+        <Col xs="8"></Col>
+        <Col></Col>
+        <Col></Col>
+    </Row>
+    </Container>
+
+     );
+}
+
+export default Keypad;
